2760a5eb9c29e849d431856806c213d5fcc63c32
Ida Ebkes <contact@ida-ebkes.eu>
2018-09-19 11:06:14 +0200

    Include CHANGES in long_description of pckg. Fixes #5.

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ version = '0.5.dev0'
 
 setup(name='adi.trash',
       version=version,
-      description="Plone addon, changing deletion-behaviour: Moves items into a trash-folder, instead of deleting them.",
-      long_description=open("README.rst").read(),
+      description="Plone addon, changing deletion-behaviour: " + 
+                  "Moves items into a trash-folder, instead of deleting them.",
+      long_description=open("README.rst").read() + '\n' + 
+                       open('docs/CHANGES.rst'),
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
